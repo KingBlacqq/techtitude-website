3b6d2445aa81c5576bdba8b14c998d2381eeacf7
KingBlacqq <Leostech26@gmail.com>
2023-11-12 07:25:22 -1200

    update

--- src/MyComponents/footerSection.jsx
+++ src/MyComponents/footerSection.jsx
@@ -17,15 +17,21 @@ const footerSection = function () {
           </div>
           <p>
             7 Adedotun Dina, Mende, Maryland, Lagos - Nigeria.
-            admin@techtitude.org<br/> 09063940977
+            admin@techtitude.org
+            <br /> 09063940977
           </p>
         </div>
+
         <div className="news-letter">
           <div className="caption">
             <p>Like to receive our Newsletter?</p>
           </div>
           <div className="input">
-            <input type="email" placeholder="Enter your Email here" id="email" />
+            <input
+              type="email"
+              placeholder="Enter your Email here"
+              id="email"
+            />
             <button className="newsletter-button">READ MORE</button>
           </div>
           <div className="footer-icon">
@@ -33,14 +39,45 @@ const footerSection = function () {
           </div>
         </div>
       </div>
-      {/* <div className="footer-address"> */}
 
-      {/* </div> */}
-      <div className="copyright-footer">
-      <hr />
-      <div className="copy-right">
-        <p>TECHTITUDE © 2023 - All Right reserved!</p>
+      {/* MOBILE NAV-LINKS AND NEWSLETTER */}
+      <div className="mobile-section">
+        <div className="mobile-layout">
+          <div className="mobile-nav">
+            <a href="#">COMPANY</a>
+            <a href="#">COURSES</a>
+            <a href="#">RESOURCES</a>
+          </div>
+          <p>
+            7 Adedotun Dina, Mende, Maryland, Lagos - Nigeria.
+            admin@techtitude.org
+            <br /> 09063940977
+          </p>
+        </div>
+
+        <div className="mobile-newsletter">
+          <div className="mobile-caption">
+            <p>Like to receive our Newsletter?</p>
+          </div>
+          <div className="input">
+            <input
+              type="email"
+              placeholder="Enter your Email here"
+              id="email"
+            />
+            <button className="newsletter-button">SUBSCRIBE</button>
+          </div>
+
+          <div className="mobile-socialmedia">
+            <img src={SOCIALMEDIA_ICON} alt="SOCIALMEDIA ICON" />
+          </div>
+        </div>
       </div>
+      <div className="copyright-footer">
+        <hr />
+        <div className="copy-right">
+          <p>TECHTITUDE © 2023 - All Right reserved!</p>
+        </div>
       </div>
     </div>
   );
